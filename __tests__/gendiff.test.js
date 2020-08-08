7dadbcd893422fda09bc71fb660226920a9c4f55
Pavel Sirotkin <pav.pnz@gmail.com>
2020-08-08 23:54:23 +0200

    f

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-undef */
 import genDiff from '../src/gendiff.js';
 
 test('diff 1', () => {
-    expect(genDiff('./file1.json', './file2.json')).toEqual(`
+  expect(genDiff('./__tests__/file1.json', './__tests__/file2.json')).toEqual(`
 {
 - follow: false
-    host: hexlet.io
+  host: hexlet.io
 - proxy: 123.234.53.22
 - timeout: 50
 + timeout: 20
 + verbose: true
-}`
-);
-    
+}`);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
+import path from 'path';
 
 export default (filepath1, filepath2) => {
-    const f1 = fs.readFileSync(filepath1)
+  const f1 = fs.readFileSync(path.resolve(filepath1), { encoding: 'utf-8' });
+  const f2 = fs.readFileSync(path.resolve(filepath2), { encoding: 'utf-8' });
+  console.log('F1 -> ', f1);
+  console.log('F2 -> ', f2);
 }
