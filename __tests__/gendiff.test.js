91e5a14a320d1da2c9cf84201e139007ed7da90d
Pavel Sirotkin <pav.pnz@gmail.com>
2020-08-23 13:55:19 +0200

    refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,128 +8,268 @@ const __filename = fileURLToPath(import.meta.url);
 /* eslint-disable no-underscore-dangle */
 const __dirname = path.dirname(__filename);
 
-test('diff flat json files', () => {
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('diff yaml files', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.json'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.json'),
+      path.resolve(getFixturePath('file1.yaml')),
+      path.resolve(getFixturePath('file2.yaml')),
     ),
   ).toEqual(`
 {
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: too much
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        fee: 100500
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+    }
 }`);
 });
 
-test('diff flat yaml files', () => {
+test('diff yaml/json files 1', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.yaml'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.yaml'),
+      path.resolve(getFixturePath('file1.yaml')),
+      path.resolve(getFixturePath('file2.json')),
     ),
   ).toEqual(`
 {
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
-});
-
-test('diff flat yaml/json files 1', () => {
-  expect(
-    genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.yaml'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.json'),
-    ),
-  ).toEqual(`
-{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
-});
-
-test('diff flat yaml/json files 2', () => {
-  expect(
-    genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.json'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.yaml'),
-    ),
-  ).toEqual(`
-{
-  + follow: false
-    host: hexlet.io
-  + proxy: 123.234.53.22
-  - timeout: 20
-  + timeout: 50
-  - verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: too much
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        fee: 100500
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+    }
 }`);
 });
 
 test('diff flat json two equals files', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.json'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.json'),
+      path.resolve(getFixturePath('file1.json')),
+      path.resolve(getFixturePath('file1.json')),
     ),
   ).toEqual(`
 {
-    follow: false
-    host: hexlet.io
-    proxy: 123.234.53.22
-    timeout: 50
+    common: {
+        setting1: Value 1
+        setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow: too much
+            }
+            key: value
+        }
+    }
+    group1: {
+        baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
+    group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
 }`);
 });
 
 test('diff jsons without extention', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1_1'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.json'),
+      path.resolve(getFixturePath('file1_1')),
+      path.resolve(getFixturePath('file2.json')),
     ),
   ).toEqual(`
 {
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: too much
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        fee: 100500
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+    }
 }`);
 });
 
 test('diff inis', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file1.ini'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file2.ini'),
+      path.resolve(getFixturePath('file1.ini')),
+      path.resolve(getFixturePath('file2.ini')),
     ),
   ).toEqual(`
 {
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: too much
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        fee: 100500
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+    }
 }`);
 });
 
-test('diff nested jsons', () => {
+test('diff jsons', () => {
   expect(
     genDiff(
-      path.resolve(__dirname, '..', '__fixtures__', 'file_nested1.json'),
-      path.resolve(__dirname, '..', '__fixtures__', 'file_nested2.json'),
+      path.resolve(getFixturePath('file1.json')),
+      path.resolve(getFixturePath('file2.json')),
     ),
   ).toEqual(`
 {
---
+++ src/gendiff.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-param-reassign */
+import _ from 'lodash';
+import parser from './parsers.js';
+import stylish from './stylish.js';
+
+export default (filepath1, filepath2) => {
+  const f1 = parser(filepath1);
+  const f2 = parser(filepath2);
+
+  const getType = (value1, value2) => {
+    if (value1 === undefined) {
+      return 'added';
+    }
+
+    if (value2 === undefined) {
+      return 'deleted';
+    }
+
+    if (value1 !== value2) {
+      return 'modified';
+    }
+
+    return 'same';
+  };
+
+  const getDeletedRootKeys = (file1, file2) => (Object
+    .entries(file1)
+    .filter((entry) => !_.has(file2, entry[0]))
+    .map((entry) => {
+      const [key, value] = entry;
+      return {
+        name: key,
+        type: 'deleted',
+        value: {
+          old: value,
+        },
+      };
+    }));
+
+  const prepareValues = (value1, value2) => ({ old: value1, new: value2 });
+
+  const completeNode = (node, value1, value2) => {
+    node.type = getType(value1, value2);
+    node.value = prepareValues(value1, value2);
+    return node;
+  };
+
+  const iter = (acc, name, curPath2 = [], trails = new Set()) => {
+    curPath2.push(name);
+    const p = curPath2.join('/');
+    if (trails.has(p)) {
+      curPath2.pop();
+      return acc;
+    }
+    trails.add(p);
+    const node = { name };
+    const [valueFromFile1, valueFromFile2] = [_.get(f1, curPath2), _.get(f2, curPath2)];
+
+    if ((_.isPlainObject(valueFromFile1)) && (_.isPlainObject(valueFromFile2))) {
+      const curPath1 = [...curPath2];
+      node.children = [...Object
+        .keys(valueFromFile2)
+        .reduce((curAcc, item) => iter(curAcc, item, curPath2, trails), []), ...Object
+        .keys(valueFromFile1)
+        .reduce((curAcc, item) => iter(curAcc, item, curPath1, trails), [])];
+      acc.push(node);
+      return acc;
+    }
+
+    acc.push(completeNode(node, valueFromFile1, valueFromFile2));
+    curPath2.pop();
+    return acc;
+  };
+
+  const inplaceSortByName = (obj) => {
+    if (_.has(obj, 'children')) {
+      obj.children = _.sortBy(obj.children, (o) => o.name);
+      return obj.children.map(inplaceSortByName);
+    }
+    return obj;
+  };
+  const ast = _.sortBy(Object
+    .keys(f2)
+    .reduce((acc, key) => iter(acc, key), getDeletedRootKeys(f1, f2)), (o) => o.name);
+
+  ast.map(inplaceSortByName);
+  return `\n${stylish(ast)}`;
+};
