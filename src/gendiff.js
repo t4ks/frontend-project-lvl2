58ad445c8ef2236d3c410a2c50d679ebae2d3bc6
Pavel Sirotkin <pav.pnz@gmail.com>
2020-09-08 23:09:11 +0200

    fixes

--- src/gendiff.js
+++ src/gendiff.js
@@ -8,57 +8,60 @@ export default (config1, config2, format = 'stylish') => {
   const configNew = parse(config2);
   const formatter = getFormatter(format);
 
-  const makeNode = (name, {
-    type = null, oldValue = null, newValue = null, children = [],
-  } = {}) => ({
-    [null]: { name, children },
-    deleted: { name, type, value: { old: oldValue } },
-    modified: { name, type, value: { old: oldValue, new: newValue } },
-    added: { name, type, value: { new: newValue } },
-    same: { name, type, value: { old: oldValue, new: newValue } },
-  }[type]);
+  const compare = (configBefore, configAfter) => {
+    const keys = _.union(Object.keys(configBefore), Object.keys(configAfter)).sort();
 
-  const compare = (configBefore, configAfter, result = []) => {
-    Object.entries(configBefore).forEach((entry) => {
-      const [key, oldValue] = entry;
+    return keys.map((key) => {
       const newValue = configAfter[key];
+      const oldValue = configBefore[key];
 
       if (newValue !== undefined && _.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
-        result.push(makeNode(key));
-        return compare(oldValue, newValue, _.last(result).children);
+        return {
+          name: key,
+          children: compare(oldValue, newValue),
+        };
       }
 
       if (newValue === undefined) {
-        result.push(makeNode(key, { type: 'deleted', oldValue }));
-      } else if (oldValue !== newValue) {
-        result.push(makeNode(key, { type: 'modified', oldValue, newValue }));
-      } else if (oldValue === configAfter[key]) {
-        result.push(makeNode(key, { type: 'same', oldValue, newValue }));
+        return {
+          name: key,
+          type: 'deleted',
+          value: {
+            old: oldValue,
+          },
+        };
+      }
+      if (oldValue === undefined) {
+        return {
+          name: key,
+          type: 'added',
+          value: {
+            new: newValue,
+          },
+        };
+      }
+      if (oldValue !== newValue) {
+        return {
+          name: key,
+          type: 'modified',
+          value: {
+            old: oldValue,
+            new: newValue,
+          },
+        };
       }
 
-      return result;
+      return {
+        name: key,
+        type: 'same',
+        value: {
+          old: oldValue,
+          new: newValue,
+        },
+      };
     });
-
-    Object.entries(configAfter).filter((entry) => {
-      const [key] = entry;
-      return !_.has(configBefore, key);
-    }).forEach((entry) => {
-      const [key, newValue] = entry;
-      return result.push(makeNode(key, { type: 'added', newValue }));
-    });
-
-    return result;
   };
 
-  const inplaceSortByName = (obj) => {
-    if (_.has(obj, 'children')) {
-      obj.children = _.sortBy(obj.children, (o) => o.name);
-      return obj.children.map(inplaceSortByName);
-    }
-    return obj;
-  };
   const ast = compare(configOld, configNew);
-  ast.map(inplaceSortByName);
-
   return `\n${formatter(ast)}\n`;
 };
