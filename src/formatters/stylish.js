161ac777594dfce80c939dd6c526d455e32021a4
Pavel Sirotkin <pav.pnz@gmail.com>
2020-09-21 23:50:23 +0200

    f

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,32 +4,33 @@ const addSymbol = '+';
 const deleteSymbol = '-';
 const spaceSymbol = ' ';
 const emptySymbol = '';
+const indents = 4;
 
 const makeIndent = (symbol) => (symbol === emptySymbol ? emptySymbol : `${symbol}${spaceSymbol}`);
 
-const makeDepthIndent = (symbol, depth, indents) => (
+const makeDepthIndent = (symbol, depth) => (
   symbol === emptySymbol
     ? `${spaceSymbol.repeat(Math.imul(indents, depth))}`
     : `${spaceSymbol.repeat(Math.imul(indents, depth) - 2)}`);
 
-const makeRow = (symbol, name, value, depth, indents) => {
+const makeRow = (symbol, name, value, depth) => {
   const indent = makeIndent(symbol);
-  const depthIndents = makeDepthIndent(symbol, depth, indents);
+  const depthIndents = makeDepthIndent(symbol, depth);
   const key = `${depthIndents}${indent}${name}`;
   if (_.isPlainObject(value)) {
     return `${key}: {\n${Object.entries(value)
-      .map((entry) => makeRow(emptySymbol, ...entry, depth + 1, indents))
+      .map((entry) => makeRow(emptySymbol, ...entry, depth + 1))
       .join('')}${spaceSymbol.repeat(depthIndents.length + indent.length)}}\n`;
   }
 
   return `${key}: ${value}\n`;
 };
 
-const formatNode = (node, depth = 1, indents = 4) => {
+const formatNode = (node, depth = 1) => {
   const depthIndents = spaceSymbol.repeat(indents * depth);
   switch (node.type) {
     case 'nested':
-      return `${depthIndents}${node.name}: {\n${node.children.map((n) => formatNode(n, depth + 1, indents)).join('')}${depthIndents}}\n`;
+      return `${depthIndents}${node.name}: {\n${node.children.map((n) => formatNode(n, depth + 1)).join('')}${depthIndents}}\n`;
     case 'added':
       return makeRow(addSymbol, node.name, node.newValue, depth, indents);
     case 'deleted':
