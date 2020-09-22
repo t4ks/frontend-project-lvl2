2305b38c20cc860a178f44946718ab40c0f1640d
Pavel Sirotkin <pav.pnz@gmail.com>
2020-09-22 23:23:25 +0200

    refactor plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,19 +14,22 @@ const printValue = (value) => {
 
 const filterEmptyString = (s) => (s !== null);
 
-const formatNode = (elem, curPath = []) => {
-  switch (elem.type) {
-    case 'nested':
-      return elem.children.map((el) => formatNode(el, [...curPath, elem.name])).filter(filterEmptyString).join('\n');
-    case 'modified':
-      return `Property '${[...curPath, elem.name].join('.')}' was updated. From ${printValue(elem.oldValue)} to ${printValue(elem.newValue)}`;
-    case 'deleted':
-      return `Property '${[...curPath, elem.name].join('.')}' was removed`;
-    case 'added':
-      return `Property '${[...curPath, elem.name].join('.')}' was added with value: ${printValue(elem.newValue)}`;
-    default:
-      return null;
-  }
+const plain = (ast) => {
+  const iter = (tree, curPath = []) => tree.map((elem) => {
+    switch (elem.type) {
+      case 'nested':
+        return iter(elem.children, [...curPath, elem.name]);
+      case 'modified':
+        return `Property '${[...curPath, elem.name].join('.')}' was updated. From ${printValue(elem.oldValue)} to ${printValue(elem.newValue)}`;
+      case 'deleted':
+        return `Property '${[...curPath, elem.name].join('.')}' was removed`;
+      case 'added':
+        return `Property '${[...curPath, elem.name].join('.')}' was added with value: ${printValue(elem.newValue)}`;
+      default:
+        return null;
+    }
+  }).filter(filterEmptyString).join('\n');
+  return iter(ast);
 };
 
-export default (ast) => ast.map((el) => formatNode(el)).filter(filterEmptyString).join('\n');
+export default plain;
