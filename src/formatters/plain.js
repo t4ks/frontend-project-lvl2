63e051391edda02b89bbafbe325b0712a8de73e0
Pavel Sirotkin <pav.pnz@gmail.com>
2020-09-24 00:02:13 +0200

    f

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -16,18 +16,20 @@ const filterEmptyString = (s) => (s !== null);
 const buildPath = (pathSegments) => pathSegments.join('.');
 
 const plain = (ast) => {
-  const iter = (tree, curPath = []) => tree.map((elem) => {
+  const iter = (tree, pathSegments = []) => tree.map((elem) => {
     switch (elem.type) {
       case 'nested':
-        return iter(elem.children, [...curPath, elem.name]);
+        return iter(elem.children, [...pathSegments, elem.name]);
       case 'modified':
-        return `Property '${buildPath([...curPath, elem.name])}' was updated. From ${printValue(elem.oldValue)} to ${printValue(elem.newValue)}`;
+        return `Property '${buildPath([...pathSegments, elem.name])}' was updated. From ${printValue(elem.oldValue)} to ${printValue(elem.newValue)}`;
       case 'deleted':
-        return `Property '${buildPath([...curPath, elem.name])}' was removed`;
+        return `Property '${buildPath([...pathSegments, elem.name])}' was removed`;
       case 'added':
-        return `Property '${buildPath([...curPath, elem.name])}' was added with value: ${printValue(elem.newValue)}`;
-      default:
+        return `Property '${buildPath([...pathSegments, elem.name])}' was added with value: ${printValue(elem.newValue)}`;
+      case 'same':
         return null;
+      default:
+        throw Error(`Unknown type=${elem.type}`);
     }
   }).filter(filterEmptyString).join('\n');
   return iter(ast);
